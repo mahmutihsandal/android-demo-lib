f395deff514eafd89c18a460f29a004d7c46e757
Mahmutihsandal <mahmutihsandal@gmail.com>
2025-01-28 14:08:55 +0300

    Added aar dependencies

--- android-demo-lib/src/main/java/com/cynte/android_demo_lib/Hello.java
+++ android-demo-lib/src/main/java/com/cynte/android_demo_lib/Hello.java
@@ -2,6 +2,10 @@ package com.cynte.android_demo_lib;
 
 import android.util.Log;
 
+import com.cynte.test_lib.DateUtil;
+
+import java.util.Date;
+
 public class Hello {
     public static void main(String[] args) {
         /// Main Method
@@ -9,6 +13,7 @@ public class Hello {
     }
     /// Prints Message with Tag in Logcat!
     public static void printLog(String tag, String message) {
+        Log.d(tag, DateUtil.getDate());
         Log.d(tag, message);
     }
 }
